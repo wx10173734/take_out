419502a32b6f9929d593a4de14ec40a2c63d2477
Zicos <1179382172@qq.com>
2023-03-15 15:13:18 +0800

    :sparkles:完善全局异常处理器

---
+++ src/main/java/com/lzc/common/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.lzc.common;
+
+/**
+ * @title: GlobalExceptionHandler
+ * @Author luozouchen
+ * @Date: 2023/3/15 14:57
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理
+ */
+@RestControllerAdvice(annotations = {RestController.class, Controller.class})
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 异常处理方法
+     *
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+}
