391e14d88f211c420c1777fda477968d9b2bdb78
Zicos <1179382172@qq.com>
2023-03-15 16:20:43 +0800

    :sparkles:禁用员工账号，并使用扩展消息转换器解决id不一致问题。解决方法：使用扩展消息转换器将Jackson将对象转为json对象

---
+++ src/main/java/com/lzc/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.lzc.config;
+
+/**
+ * @title: MybatisPlusConfig
+ * @Author luozouchen
+ * @Date: 2023/3/15 15:23
+ */
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP的分页插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
