13badb60cad248283f0fab1c388cdf518fceed2d
Zicos <1179382172@qq.com>
2023-03-27 14:32:52 +0800

    :sparkles:完成移动端购物车添加功能

--- src/main/java/com/lzc/controller/ShoppingCartController.java
+++ src/main/java/com/lzc/controller/ShoppingCartController.java
@@ -1,13 +1,13 @@
 package com.lzc.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.lzc.common.BaseContext;
 import com.lzc.common.R;
 import com.lzc.entity.ShoppingCart;
 import com.lzc.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +26,45 @@ public class ShoppingCartController {
     @GetMapping("/list")
     public R<List<ShoppingCart>> list() {
         return null;
+    }
+
+
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("购物车数据,{}", shoppingCart);
+        //设置用户id,指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        Long dishId = shoppingCart.getDishId();
+        Long setmealId = shoppingCart.getSetmealId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+        if (dishId != null) {
+            //添加到购物车的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            //添加到购物车的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, setmealId);
+
+        }
+        //sql:select * from shopping_cart where user_id = ? and dish_id/setmeal_id=?
+        //查询当前菜品或套餐是否在购物车中
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        //如果已经存在，就在原来基础上加1
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            //如果不存在，则添加到购物车，数量默认为1
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+
+        return R.success(cartServiceOne);
     }
 
 
